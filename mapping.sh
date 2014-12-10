fb8de2a6ec0b5c7228672cc7b19a8b4e5f704abd
Akihiko Horiuchi <12ff5b8@gmail.com>
2014-12-10 13:28:02 +0900

    mappingも修正

--- mapping.sh
+++ mapping.sh
@@ -10,11 +10,6 @@ curl -XPUT $URL/_template/sensu-metrics -d '{
       "numeric_detection" : true,
       "properties": {
         "@timestamp": { "type":"date", "format":"dateOptionalTime" },
-        "client": {type: "string"},
-        "address": {type: "string"},
-        "check": {type: "string"},
-        "status": {type: "integer"},
-        "command": {type: "string"},
         "key": {type: "string"},
         "value": {type: "float"}
         }
